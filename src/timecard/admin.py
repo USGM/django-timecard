c51c520bd781a34dc58bc119e1865041a82bca88
Michael Sandford <sandford@ufl.edu>
2012-08-27 15:43:01 -0500

    Making it possible to see hours

--- src/timecard/admin.py
+++ src/timecard/admin.py
@@ -3,8 +3,11 @@ from django.contrib import admin
 from timecard.models import Entry
 
 class EntryAdmin(admin.ModelAdmin):
-	list_display = ('date', 'user', 'start_time', 'end_time', 'difference')
+	list_display = ('date', 'user', 'start_time', 'end_time', 'difference_hours')
 	list_filter = ('user',)
 	date_hierarchy = 'date'
+	
+	def difference_hours(self, entry):
+		return float(entry.difference) / 60.0
 
 admin.site.register(Entry, EntryAdmin)
