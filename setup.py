4863b06115d72d90cbefefe7062f460e98f5bd98
Jonathan Piacenti <jpiacenti@usglobalmail.com>
2013-10-01 11:49:51 -0500

    Added the ability to mark entries paid or upcoming in bulk.

--- setup.py
+++ setup.py
@@ -3,35 +3,32 @@ import os
 from setuptools import setup, find_packages
 
 def read(fname):
-	return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-	name = 'django-timecard',
-	version = '0.2',
-	url = 'http://github.com/myles/django-timecard',
-	license = 'BSD License',
-	description = 'A Django timecard application.',
-	long_description = read('README'),
+    name = 'django-timecard',
+    version = '0.3',
+    url = 'http://github.com/USGM/django-timecard',
+    license = 'BSD License',
+    description = 'A Django timecard application.',
+    long_description = read('README'),
 	
-	author = 'Myles Braithwaite',
-	author_email = 'me@mylesbraithwaite.com',
+    author = 'Myles Braithwaite',
+    author_email = 'me@mylesbraithwaite.com',
 	
-	packages = find_packages('src'),
-	package_dir = {'': 'src'},
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
 	
-	install_requires = [
-		'setuptools',
-		'python-dateutil',
-    'isoweek',
-  ],
+    install_requires = [
+        'setuptools',
+        'python-dateutil',
+        'isoweek'],
 	
-	classifiers = [
-		'Development Status :: 4 - Beta',
-		'Framework :: Django',
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: BSD License',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python',
-		'Topic :: Internet :: WWW/HTTP',
-	],
-)
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP'],)
--- src/timecard/admin.py
+++ src/timecard/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 from timecard.models import Entry
 
+def mark_paid(modeladmin, request, queryset):
+    """
+    Marks a queryset of entries as paid.
+    """
+    queryset.update(status=Entry.PAID)
+
+mark_paid.short_description = "Mark these entries as paid."
+
+def mark_upcoming(modeladmin, request, queryset):
+    """
+    Marks a queryset of entries as upcoming.
+    """
+    queryset.update(status=Entry.UPCOMING)
+
+mark_upcoming.short_description = "Mark these entries as uncoming."
+
 class EntryAdmin(admin.ModelAdmin):
-	list_display = ('date', 'user', 'start_time', 'end_time', 'hours', 'status', )
-	list_filter = ('status', 'user',)
-	date_hierarchy = 'date'
+    list_display = ('date', 'user', 'start_time', 'end_time', 'hours', 'status', )
+    list_filter = ('status', 'user',)
+    date_hierarchy = 'date'
+    actions = [mark_paid, mark_upcoming]
 
 admin.site.register(Entry, EntryAdmin)
